b210288e1a84d182c6580cdac88cba30c664a872
mattyan1053 <h.matsumaruka14161@gmail.com>
2019-03-03 03:54:13 +0900

    アプリケーションのロード時のエラーを出力してわかりやすくなるよう修正

--- CCS_Launcher/InfoLoader.cpp
+++ CCS_Launcher/InfoLoader.cpp
@@ -5,6 +5,7 @@ AppData InfoLoader::loadAppData(const FilePath& path) {
 
 	Array<FilePath> fileList = FileSystem::DirectoryContents(path);
 	AppData appData;
+	bool screenshotFlag = false, infoFlag = false, readmeFlag = false;
 	m_isMovieExist = false;
 
 	Font font(10, Typeface::Default);
@@ -14,12 +15,14 @@ AppData InfoLoader::loadAppData(const FilePath& path) {
 		// screenshotのロード
 		if (file.includes(L"screenshot") || file.includes(L"Screenshot") || file.includes(L"ScreenShot")) {
 			appData.screenshot = Texture(file);
+			screenshotFlag = true;
 			continue;
 		}
 
 		// 実行ファイル名、ジャンル、操作方法のロード
 		if (file.includes(L"info") || file.includes(L"Info")) {
 			TextReader reader(file);
+			infoFlag = true;
 			reader.readLine(appData.executePath);
 			reader.readLine(appData.kind);
 			reader.readLine(appData.usingtext);
@@ -31,6 +34,7 @@ AppData InfoLoader::loadAppData(const FilePath& path) {
 			// テキストファイル読み込み
 			TextReader reader(file);
 			String content = reader.readAll();
+			readmeFlag = true;
 
 			// イメージ変換
 			Image contentImg;
@@ -53,6 +57,31 @@ AppData InfoLoader::loadAppData(const FilePath& path) {
 
 	// 実行ファイルのパス変換
 	appData.executePath = path + appData.executePath;
+
+	// 読み込めていないファイルがあればデバッグ表示
+	if (!screenshotFlag) {
+		appData.screenshot = Texture(L"img/noimg.png");
+		Println(L"screenshotがロードできませんでした。\nアプリケーション名：" + FileSystem::BaseName(path));
+	}
+
+	if (!infoFlag) {
+		Println(L"infoがロードできませんでした。\nアプリケーション名：" + FileSystem::BaseName(path));
+	}
+	else {
+
+		// 実行ファイルが存在しなければデバッグ表示
+		if (!FileSystem::Exists(appData.executePath)) {
+			Println(L"実行ファイルが存在しません。 <" + appData.executePath + L">");
+		}
+
+		if (appData.kind == L"" || appData.usingtext == L"") {
+			Println(L"infoの内容が不正です。\nアプリケーション名：" + FileSystem::BaseName(path));
+		}
+	}
+
+	if (!readmeFlag) {
+		Println(L"readmeがロードできませんでした。\nアプリケーション名：" + FileSystem::BaseName(path));
+	}
 
 	return appData;
 
